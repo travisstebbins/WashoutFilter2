a00f2928868e828efb02d1f2e20c608e0c80181f
Travis Stebbins <stebbins.travis@gmail.com>
2017-11-30 09:24:38 -0600

    Add 360 video "Special Delivery" to project

--- WashoutFilter/Assets/Scripts/MainMenuManager.cs
+++ WashoutFilter/Assets/Scripts/MainMenuManager.cs
@@ -6,20 +6,29 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] VRStandardAssets.Utils.VRInteractiveItem rainOrShineButton;
+    [SerializeField] VRStandardAssets.Utils.VRInteractiveItem specialDeliveryButton;
 
     void OnEnable()
     {
         rainOrShineButton.OnClick += rainOrShine;
+        specialDeliveryButton.OnClick += specialDelivery;
     }
 
     void OnDisable()
     {
         rainOrShineButton.OnClick -= rainOrShine;
+        specialDeliveryButton.OnClick -= specialDelivery;
     }
 
     public void rainOrShine()
     {
         GameManager.instance.videoIndex = 0;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void specialDelivery()
+    {
+        GameManager.instance.videoIndex = 1;
         SceneManager.LoadScene("Main");
     }
 }
