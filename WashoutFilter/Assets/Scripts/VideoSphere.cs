6994afa4649185f5dcf7df2e208940ded87e1f14
Michael Bruner <michaelbruner@tamu.edu>
2018-03-09 13:40:10 -0600

    Fix bug where audio wouldn't play (add code to VideoSphere script)

--- WashoutFilter/Assets/Scripts/VideoSphere.cs
+++ WashoutFilter/Assets/Scripts/VideoSphere.cs
@@ -5,8 +5,57 @@ using UnityEngine.Video;
 
 public class VideoSphere : MonoBehaviour
 {
-	void Start ()
+	public VideoClip videoToPlay;
+
+	private VideoPlayer videoPlayer;
+	private VideoSource videoSource;
+
+	//Audio
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start()
 	{
-		GetComponent<VideoPlayer> ().clip = GameManager.instance.currentVideo;
+		Application.runInBackground = true;
+		StartCoroutine(playVideo());
+	}
+
+	IEnumerator playVideo()
+	{
+		videoToPlay = GameManager.instance.currentVideo;
+
+		videoPlayer = gameObject.GetComponent<VideoPlayer>();
+
+		audioSource = gameObject.GetComponent<AudioSource>();
+
+		//Disable Play on Awake for both Video and Audio
+		videoPlayer.playOnAwake = false;
+		audioSource.playOnAwake = false;
+
+		//We want to play from video clip not from url
+		videoPlayer.source = VideoSource.VideoClip;
+
+		//Set Audio Output to AudioSource
+		videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+		//Assign the Audio from Video to AudioSource to be played
+		videoPlayer.EnableAudioTrack(0, true);
+		videoPlayer.SetTargetAudioSource(0, audioSource);
+
+		//Set video To Play then prepare Audio to prevent Buffering
+		videoPlayer.clip = videoToPlay;
+		videoPlayer.Prepare();
+
+		//Wait until video is prepared
+		while (!videoPlayer.isPrepared)
+		{
+			yield return null;
+		}
+
+		//Play Video
+		videoPlayer.Play();
+
+		//Play Sound
+		audioSource.Play();
 	}
 }
